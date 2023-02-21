70308156b025e0193f41bfd7c09ca8ce4776b4cc
Durawix <89468508+Durawix@users.noreply.github.com>
2023-02-21 18:22:38 +0100

    Update GoogleSheet_to_Calendar_App_Script.js
    I have added the possibility to change the duration of the event, and some logs in comments that help to find issues. f. e.  If there is an issue with the calendar ID error "Invalid calendar ID" pops up.

--- GoogleSheet_to_Calendar_App_Script.js
+++ GoogleSheet_to_Calendar_App_Script.js
@@ -1,12 +1,19 @@
 function ScheduleScrims()
 {
 var spreadsheet = SpreadsheetApp.getActiveSheet();
-var calendarId = spreadsheet.getRange("C4").getValue(); 
+var calendarId = spreadsheet.getRange("G4").getValue(); 
 /*z tej komórki bierzemy info o ID*/
+ if (!calendarId) {
+    Logger.log("Invalid calendar ID");
+    return;
+  }
 var eventCal = CalendarApp.getCalendarById(calendarId);
+if (!eventCal) {
+    Logger.log("Invalid calendar object"); /* it shows if something is wrong with id of calendar*/
+    return;
+  }
 
-
-var row = spreadsheet.getRange("A6:C37").getValues();
+var row = spreadsheet.getRange("A6:H44").getValues();
 /*określamy zakres z którego bierzemy info o wartościach w komórkach*/
 /** [13/05/2022 18:00:00	Scrim vs BeKind	3 maps Villa kafe Theme]
     [14/05/2022 18:00:00	Scrim vs BB	3 maps Villa kafe Theme]
@@ -20,8 +27,15 @@ for (var x=0; x<row.length; x++) { /**x rośnie o 1 dopóki x jest mniejszy niż
   var shift = row[x]; /** shift to nazwa zmiennej a row.length (wiersz) to wartości z danych wierszy */
 
   var startTime = shift[0]; /** */
-  var subject = shift[1];
-  var description = shift[2];
+  // var start_hour = shift[3]
+  var subject = shift[5];
+  var description = shift[6];
+  var durationInHours = shift[7];
+//Logger.log('durationInHours: ' + durationInHours)
+;
+var endTime = new Date(startTime.getTime() + (durationInHours * 60 *60*1000)); // ustawiamy koniec wydarzenia na godzinę później od początku
+//DLA SPRAWDZENIA Logger.log('endTime: ' + endTime); // wyświetlamy wartość zmiennej w konsoli deweloperskiej
+
 var events = eventCal.getEventsForDay(startTime); /** weź zdarzenia w dniu startTime */
   for(var i=0; i<events.length;i++) //loop through all events
   {
@@ -32,7 +46,7 @@ if (subject!=""){
     
 
 
-  eventCal.createEvent(subject, startTime, startTime).setDescription(description); /** zdarzenia wrzuca do kalendarza i dodaje do nich opis*/
+  eventCal.createEvent(subject, startTime, endTime).setDescription(description); /** zdarzenia wrzuca do kalendarza i dodaje do nich opis*/
 
 }
  
